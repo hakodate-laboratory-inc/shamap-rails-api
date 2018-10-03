4a09f8d2915a2cfff7642b439ae3c874f728b4f3
i544c <b1015078@fun.ac.jp>
2018-10-03 17:37:21 +0900

    Fix CORS for Netlify deploy preview

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "localhost:3001",
             "https://shamap.xyz",
-            /\Ahttps:\/\/\w+--elegant-fermi-b93fc6\.netlify\.com\z/ # rubocop:disable Style/RegexpLiteral
+            /\Ahttps:\/\/deploy-preview-\d+--elegant-fermi-b93fc6\.netlify\.com\z/ # rubocop:disable Style/RegexpLiteral
 
     resource "*",
              headers: :any,
