fc66b51649b86386789a2d3105e9a5502eb2b5e5
ToshihitoKon <toshihito.kon@gmail.com>
2018-10-25 16:19:01 +0900

    cleanup authority define enum

--- app/models/authority.rb
+++ app/models/authority.rb
@@ -24,4 +24,6 @@
 class Authority < ApplicationRecord
   belongs_to :v1_map, class_name: "V1::Map"
   belongs_to :user, class_name: "User"
+  
+  enum authority: [:administrator, :contributor, :viewer]
 end
