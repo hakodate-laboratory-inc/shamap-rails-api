33fadf2cf701ab346a4a9e38b27ccd8c5d29967c
ToshihitoKon <toshihito.kon@gmail.com>
2018-10-26 17:11:08 +0900

    cleanup delete whitespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,7 +43,7 @@ class User < ApplicationRecord
   include DeviseTokenAuth::Concerns::User
 
   validates :name, length: { in: 3..16 }
-  
+
   has_many :authorities, dependent: :destroy, inverse_of: :user
   has_many :maps, through: :authorities, source: :v1_map
 end
